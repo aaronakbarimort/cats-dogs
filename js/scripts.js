16f8285357ca9d519b78498f9d98b5474adb27dc
Aaron Akbari-Mort and Nick Kardish <nick@>
2018-03-20 16:52:00 -0700

    add ability to click photo, pop up new photo in a new element, then clicking new photo deletes it.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+
+/* Cats VS. Dogs */
   $("button#meow").click(function(){
     $("ul#cat").prepend("<li>Meow!</li>");
     $("ul#dog").prepend("<li>Woof!</li>");
@@ -7,4 +9,16 @@ $(document).ready(function() {
     $("ul#cat").prepend("<li>Hiss!</li>");
     $("ul#dog").prepend("<li>Bark!</li>");
   });
+
+  /* Tom VS. Jerry */
+    $(".visible").click(function() {
+      $("#input").prepend("<p></p>");
+      $("#input").addClass("bkgrnd");
+    });
+    $(".unstyled").prepend().click(function() {
+      $(this).click(function() {
+        $(".unstyled").toggle(".initially-hidden");
+      });
+    });
+
 });
