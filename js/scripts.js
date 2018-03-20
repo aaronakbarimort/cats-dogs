08172120b5b0afa319023e22da7c6faa17cfa600
Aaron Akbari-Mort and Nick Kardish <nick@>
2018-03-20 14:50:55 -0700

    add buttons that allow cat and dog to bark and meow

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $("button#meow").click(function(){
+    $("ul#cat").prepend("<li>Meow!</li>");
+    $("ul#dog").prepend("<li>Woof!</li>");
+  });
+  $("button#bark").click(function(){
+    $("ul#cat").prepend("<li>Hiss!</li>");
+    $("ul#dog").prepend("<li>Bark!</li>");
+  });
+});
